a5edfcdd3f4d820ccc61674f56dbfa2ac93296e0
Joseph Savold <joesavold@hotmail.com>
2017-09-29 19:45:17 -0400

    Moved main code into runner class and cleaned db access
    * Added functions for database interface
    * Stock list now updates daily from database
    * Main code in Runner class

--- stock.py
+++ stock.py
@@ -10,68 +10,114 @@ import time
 import sys
 from datetime import datetime, timedelta
 
-class stock_sql_driver:
+class StockDriver:
 
 	def __init__(self):
-		self.stock_list = ['MMM','ABT','ABBV','ACN','ATVI','AYI','ADBE','AMD','AAP','AES','AET','AMG','AFL','A','APD','AKAM','ALK','ALB','ARE','ALXN','ALGN','ALLE','AGN','ADS','LNT','ALL','GOOGL','GOOG','MO','AMZN','AEE','AAL','AEP','AXP','AIG','AMT','AWK','AMP','ABC','AME','AMGN','APH','APC','ADI','ANDV','ANSS','ANTM','AON','AOS','APA','AIV','AAPL','AMAT','ADM','ARNC','AJG','AIZ','T','ADSK','ADP','AZO','AVB','AVY','BHGE','BLL','BAC','BK','BCR','BAX','BBT','BDX','BRK.B','BBY','BIIB','BLK','HRB','BA','BWA','BXP','BSX','BHF','BMY','AVGO','BF.B','CHRW','CA','COG','CDNS','CPB','COF','CAH','CBOE','KMX','CCL','CAT','CBG','CBS','CELG','CNC','CNP','CTL','CERN','CF','SCHW','CHTR','CHK','CVX','CMG','CB','CHD','CI','XEC','CINF','CTAS','CSCO','C','CFG','CTXS','CLX','CME','CMS','COH','KO','CTSH','CL','CMCSA','CMA','CAG','CXO','COP','ED','STZ','COO','GLW','COST','COTY','CCI','CSRA','CSX','CMI','CVS','DHI','DHR','DRI','DVA','DE','DLPH','DAL','XRAY','DVN','DLR','DFS','DISCA','DISCK','DISH','DG','DLTR','D','DOV','DWDP','DPS','DTE','DRE','DUK','DXC','ETFC','EMN','ETN','EBAY','ECL','EIX','EW','EA','EMR','ETR','EVHC','EOG','EQT','EFX','EQIX','EQR','ESS','EL','ES','RE','EXC','EXPE','EXPD','ESRX','EXR','XOM','FFIV','FB','FAST','FRT','FDX','FIS','FITB','FE','FISV','FLIR','FLS','FLR','FMC','FL','F','FTV','FBHS','BEN','FCX','GPS','GRMN','IT','GD','GE','GGP','GIS','GM','GPC','GILD','GPN','GS','GT','GWW','HAL','HBI','HOG','HRS','HIG','HAS','HCA','HCP','HP','HSIC','HSY','HES','HPE','HLT','HOLX','HD','HON','HRL','HST','HPQ','HUM','HBAN','IDXX','INFO','ITW','ILMN','IR','INTC','ICE','IBM','INCY','IP','IPG','IFF','INTU','ISRG','IVZ','IRM','JEC','JBHT','SJM','JNJ','JCI','JPM','JNPR','KSU','K','KEY','KMB','KIM','KMI','KLAC','KSS','KHC','KR','LB','LLL','LH','LRCX','LEG','LEN','LVLT','LUK','LLY','LNC','LKQ','LMT','L','LOW','LYB','MTB','MAC','M','MRO','MPC','MAR','MMC','MLM','MAS','MA','MAT','MKC','MCD','MCK','MDT','MRK','MET','MTD','MGM','KORS','MCHP','MU','MSFT','MAA','MHK','TAP','MDLZ','MON','MNST','MCO','MS','MOS','MSI','MYL','NDAQ','NOV','NAVI','NTAP','NFLX','NWL','NFX','NEM','NWSA','NWS','NEE','NLSN','NKE','NI','NBL','JWN','NSC','NTRS','NOC','NRG','NUE','NVDA','ORLY','OXY','OMC','OKE','ORCL','PCAR','PKG','PH','PDCO','PAYX','PYPL','PNR','PBCT','PEP','PKI','PRGO','PFE','PCG','PM','PSX','PNW','PXD','PNC','RL','PPG','PPL','PX','PCLN','PFG','PG','PGR','PLD','PRU','PEG','PSA','PHM','PVH','QRVO','PWR','QCOM','DGX','Q','RRC','RJF','RTN','O','RHT','REG','REGN','RF','RSG','RMD','RHI','ROK','COL','ROP','ROST','RCL','CRM','SBAC','SCG','SLB','SNI','STX','SEE','SRE','SHW','SIG','SPG','SWKS','SLG','SNA','SO','LUV','SPGI','SWK','SBUX','STT','SRCL','SYK','STI','SYMC','SYF','SNPS','SYY','TROW','TGT','TEL','FTI','TXN','TXT','TMO','TIF','TWX','TJX','TMK','TSS','TSCO','TDG','TRV','TRIP','FOXA','FOX','TSN','UDR','ULTA','USB','UA','UAA','UNP','UAL','UNH','UPS','URI','UTX','UHS','UNM','VFC','VLO','VAR','VTR','VRSN','VRSK','VZ','VRTX','VIAB','V','VNO','VMC','WMT','WBA','DIS','WM','WAT','WEC','WFC','HCN','WDC','WU','WRK','WY','WHR','WMB','WLTW','WYN','WYNN','XEL','XRX','XLNX','XL','XYL','YUM','ZBH','ZION','ZTS']
+		self.update_stock_list()
 
+	# open connection to the database
+	def connect_to_db(self):
+		conn = mysql.connector.connect(
+			user = 'root', 
+			password='f9nuF66m+', 
+			unix_socket='/var/run/mysqld/mysqld.sock', 
+			database='stock_info'
+		)
+		return conn
+
+	# get list of stocks from the database
+	def update_stock_list(self):	
+	
+		print("Updating Stock List...")
+
+		conn = self.connect_to_db();
+		cursor = conn.cursor()
+		cursor.execute("SELECT stockKey FROM stocks;")
+
+		# format them into a list
+		self.stock_list = []
+		for entry in cursor:
+			self.stock_list.append(entry[0])
+
+		conn.close()
+	
 	# get stock info from Robinhood module
 	def get_stock_info(self):
+
+		print("Getting stock data from Robinhood")
 
 		trader = Robinhood()
 		quotes = zip(self.stock_list, trader.quotes_data(self.stock_list))
 		return quotes
 
-
+	# take the data from all stocks in stock_list at the specified datetime
 	def take_snapshot(self, target_date):
 		data = self.get_stock_info()
-		conn = mysql.connector.connect(user = 'root', password='f9nuF66m+', unix_socket='/var/run/mysqld/mysqld.sock', database='stock_info')
+		conn = self.connect_to_db()
 		cursor = conn.cursor()
 		for i in range(len(data)):
 			if (data[i][1] == None):
 				print ("No data for: data[i][0]")
 				continue
-			sql_string = "INSERT INTO snapshots (stockKey, targetDateTime, stockAskPrice, stockAskSize, stockBidPrice, stockLastTradePrice, stockPreviousClosePrice, stockPreviousCloseDate, stockTradingHalted, stockHasTraded, stockLastTradePriceSource) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}', '{10}');".format(data[i][0],target_date,data[i][1]['ask_price'],data[i][1]['ask_size'],data[i][1]['bid_price'],data[i][1]['bid_size'],data[i][1]['last_trade_price'],data[i][1]['previous_close'],data[i][1]['previous_close_date'],data[i][1]['trading_halted'],data[i][1]['has_traded'],data[i][1]['last_trade_price_source'])
+			sql_string = ("INSERT INTO snapshots (stockKey, targetDateTime, stockAskPrice, stockAskSize, " + 
+				"stockBidPrice, stockLastTradePrice, stockPreviousClosePrice, stockPreviousCloseDate, " + 
+				"stockTradingHalted, stockHasTraded, stockLastTradePriceSource) VALUES ('{0}', '{1}', " + 
+				"'{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}', '{10}');")
+			sql_string = sql_string.format(data[i][0],target_date,data[i][1]['ask_price'],
+				data[i][1]['ask_size'],data[i][1]['bid_price'],data[i][1]['bid_size'],
+				data[i][1]['last_trade_price'],data[i][1]['previous_close'],
+				data[i][1]['previous_close_date'],data[i][1]['trading_halted'],
+				data[i][1]['has_traded'],data[i][1]['last_trade_price_source'])
 			cursor.execute(sql_string)
 		conn.commit()
 		conn.close()
 
-def snap(target_date):
-	snap.driver.take_snapshot(target_date)
-# -- to-do -- clean this up it is ugly global
-snap.driver = stock_sql_driver()
 
-def plan_day(sched):
-	# setup variables to do time calculations
-	t = datetime.today()
-	print("Planning day for ", t)
-	first_time = datetime(t.year,t.month,t.day,9,30)
-	delta = timedelta(minutes = 0)
-	
-	# schedule a snapshot for every 5 minutes between 9:30 and 4:00
-	for i in range(78):
-		sched.add_job(snap, 'date', run_date=(first_time + delta), args=[(first_time+delta)])
-		delta += timedelta(minutes = 5)
+class Runner:
 
-def run():
-	print("Begin saving stocks...")
+	def __init__(self):
+		
+		logging.basicConfig()
+		self.driver = StockDriver()
+		self.sched = BackgroundScheduler()
 
-	# basic setup
-	logging.basicConfig()
-	sched = BackgroundScheduler()
+		# run schedule daily jobs everyweek day at 8
+		self.sched.add_job(self.plan_day, 'cron', day_of_week='0-4', hour='8', args=[])
+		self.sched.start()
 
-	# run schedule daily jobs everyweek day at 8
-	sched.add_job(plan_day, 'cron', day_of_week='0-4', hour='8', args=[sched])
-	sched.start()
+		# run plan day to catch any remaining (in case of late start)
+		n = datetime.now()
+		if (n.hour > 8 or (n.hour == 8 and (n.minute > 0 or n.second > 0))):
+			self.plan_day()
 
-	# run plan day to catch any remaining (in case of late start)
-	n = datetime.now()
-	if (n.hour > 8 or (n.hour == 8 and (n.minute > 0 or n.second > 0))):
-		plan_day(sched)
+		# keep program running
+		while True:
+			sys.stdout.flush()
+			time.sleep(60)
 
-	# keep program running
-	while True:
-		sys.stdout.flush()
-		time.sleep(60)
+	# clean up on shutdown
+	def __del__(self):
+		self.sched.shutdown()
 
-run()
+	# global function for taking snapshots with static reference to stock_sql_driver
+	def snap(self, target_date):
+		self.driver.take_snapshot(target_date)
+
+	# schedule all jobs for the current day
+	def plan_day(self):
+
+		# setup variables to do time calculations
+		t = datetime.today()
+		print("Planning day for ", t)
+		first_time = datetime(t.year,t.month,t.day,9,30)
+		delta = timedelta(minutes = 0)
+
+		# update stock_list
+		self.driver.update_stock_list()	
+
+		# schedule a snapshot for every 5 minutes between 9:30 and 4:00
+		for i in range(78):
+			self.sched.add_job(self.snap, 'date', run_date=(first_time + delta), args=[(first_time+delta)])
+			delta += timedelta(minutes = 5)
+
+Runner()
